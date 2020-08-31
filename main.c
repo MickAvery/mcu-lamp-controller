8d6956441d5e79404b0450a6ae07c778fce5452e
MickAvery <mav.cuyugan@gmail.com>
2020-08-31 12:23:28 -0700

    activated serial for outputting

--- main.c
+++ main.c
@@ -16,6 +16,29 @@
 
 #include "hal.h"
 
+/**
+ * configurations related to serial
+ */
+#define UART_TX_PORT   GPIOA
+#define UART_TX_PADNUM 0
+
+#define UART_RX_PORT   GPIOA
+#define UART_RX_PADNUM 1
+
+#define UART_PIN_ALTMODE 8
+
+/**
+ * configurations related to I2S
+ */
+
+#define I2S_SD_PORT   GPIOC
+#define I2S_SD_PADNUM 3
+
+#define I2S_CLK_PORT   GPIOB
+#define I2S_CLK_PADNUM 10
+
+#define I2S_PIN_ALTMODE 5
+
 #define I2S_BUFF_SIZE 1U
 
 static uint8_t i2s_buf[I2S_BUFF_SIZE] = {0U}; /* destination buffer for I2S signals */
@@ -27,7 +50,7 @@ static const I2SConfig i2s_conf = {
   NULL /* callback */
 };
 
-/*
+/**
  * Application entry point.
  */
 int main(void) {
@@ -48,23 +71,23 @@ int main(void) {
    * Activates the serial driver 2 using the driver default configuration.
    * PA2(TX) and PA3(RX) are routed to USART2.
    */
-  sdStart(&SD2, NULL);
-  palSetPadMode(GPIOA, 2, PAL_MODE_ALTERNATE(7));
-  palSetPadMode(GPIOA, 3, PAL_MODE_ALTERNATE(7));
+  sdStart(&SD4, NULL);
+  palSetPadMode(UART_TX_PORT, UART_TX_PADNUM, PAL_MODE_ALTERNATE(UART_PIN_ALTMODE));
+  palSetPadMode(UART_RX_PORT, UART_RX_PADNUM, PAL_MODE_ALTERNATE(UART_PIN_ALTMODE));
 
   /*
    * Activate I2S peripheral
    */
   i2sStart(&I2SD2, &i2s_conf);
-  palSetPadMode(GPIOC, 3, PAL_MODE_ALTERNATE(5));
-  palSetPadMode(GPIOB, 10, PAL_MODE_ALTERNATE(5));
+  palSetPadMode(I2S_SD_PORT, I2S_SD_PADNUM, PAL_MODE_ALTERNATE(I2S_PIN_ALTMODE));
+  palSetPadMode(I2S_CLK_PORT, I2S_CLK_PADNUM, PAL_MODE_ALTERNATE(I2S_PIN_ALTMODE));
 
   /*
    * Normal main() thread activity, in this demo it just performs
    * a shell respawn upon its termination.
    */
   while (true) {
-    chnWriteTimeout(&SD2, (uint8_t *)"Hello World!\r\n", 14, TIME_INFINITE);
+    chnWriteTimeout(&SD4, (uint8_t *)"Hello World!\r\n", 14, TIME_INFINITE);
 
     palSetPad(GPIOD, GPIOD_LED3);       /* Orange.  */
     osalThreadSleepMilliseconds(500);
